7f7731160868890e51e4f3f1586d62364ed3d1a1
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-13 23:20:50 +0630

    Update ml.py

--- pages/ml.py
+++ pages/ml.py
@@ -5,9 +5,7 @@ from sklearn.model_selection import train_test_split
 rfmodel= st.sidebar.checkbox('Decision Trees')
 data = pd.read_csv('career.csv')
 
-if rfmodel:
-    
-    with st.form("my_form1"):
+
         
         st.title('Classify the new career prediction')
         st.subheader("Please Choose all features for predicting good result")
@@ -33,38 +31,4 @@ if rfmodel:
         mbti = st.radio("Are you Inrovert:",['no','yes'])
     
         
-        st.write("You selected:", "Age:" + agegp + "Student:" + student)
-    
-    
-        
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            #st.write("slider", slider_val, "checkbox", checkbox_val)
-            inputdata = {'age': agegp,
-                        'studen': student, 
-                        'income': income,
-                        'credit rating': credit_rating}
-            features = pd.DataFrame(inputdata, index=[0])
-            features_dummy = pd.get_dummies(features)
-            #st.write(features)
-            #################################################
-            
-            X = data[['age', 'income', 'studen', 'credit rating']]
-            y = data[['class']]
-            X_dummies = pd.get_dummies(X)
-            X_train, X_test, y_train, y_test = train_test_split(X_dummies,y,test_size=0.25, random_state=25)
-            ##################################################
-            # load model
-            filename = 'C:/Users/Dell/Documents/tytsan/prjsample/test1'
-            loaded_model = pickle.load(open(filename, "rb"))
-            testsdata1=pd.get_dummies(features_dummy)
-            testsdata2 =  testsdata1.reindex(columns =  X_train.columns, fill_value=0)
-            clfres = loaded_model.predict(testsdata2)
-            if clfres == 'yes':
-                st.write('The customer will buy the computer')
-            else:
-                st.write('The customer will not buy the computer')
-            
-           
-            
-          
+       
