3c0ea1da4e1fdfd9b3756502427121d31ca91d67
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-16 17:03:14 +0630

    Update MachineLearning.py

--- pages/MachineLearning.py
+++ pages/MachineLearning.py
@@ -17,14 +17,14 @@ wshop = st.selectbox("Workshop:", ['testing', 'database security', 'game develop
 rw_skill = st.radio("Read and Write Skill Excellent=0,Medium=1,Poor=2", [0,1,2])
 memory_score = st.radio("Memory capability Excellent=0,Medium=1,Poor=2",[0,1,2])
 like_subj = st.selectbox("Interesting Subjects:",data['Interested subjects'].unique())
-like_area = st.selectbox("Interesting Career Area",
+like_area = st.number_input("Interesting Career Area",0,5)
 like_book = st.number_input('Pick a number code for interested type of book',0,30)
-mana_tech = st.radio("Tech or Manage:",['Management','Technical'])
-worker = st.radio("Hard or Smart:",['smart worker', 'hard worker'])
+mana_tech = st.radio("Tech=1 or Manage=0:",[0,1])
+worker = st.radio("Hard=1 or Smart=0:",[0,1])
 team = st.radio("work in team ever?if yes choice 1",[1,0])
 mbti = st.radio("Are you Inrovert:if yes choice 1",[1,0])
 st.write("Your Logic, Hackaton, Coding Skill and Public Speaking points:",logic_rate,hack_rate,code_rate,speak_rate)
 st.write("Your selected:",self_learn , extra , certificate , workshop,rw_skill,memory_score,like_subj,like_area,like_com,
-         takein_snr,like_book,+mana_tech,worker,team,mbti)
+         takein_snr,like_book,mana_tech,worker,team,mbti)
         
        
