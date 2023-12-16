c25e397c708a312c7408d4cb9d67d1b05c4d7a46
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-16 17:22:39 +0630

    Update MachineLearning.py

--- pages/MachineLearning.py
+++ pages/MachineLearning.py
@@ -18,13 +18,15 @@ rw_skill = st.radio("Read and Write Skill Excellent=0,Medium=1,Poor=2", [0,1,2])
 memory_score = st.radio("Memory capability Excellent=0,Medium=1,Poor=2",[0,1,2])
 like_subj = st.number_input("Interesting Subjects:",0,9)
 like_area = st.number_input("Interesting Career Area",0,5)
+takein_snr = st.radio("Take in input from senior IF yes=1 or 0",[0,1])
+like_com = st.number_input("Type of company choice",0,9)
 like_book = st.number_input('Pick a number code for interested type of book',0,30)
 mana_tech = st.radio("Tech=1 or Manage=0:",[0,1])
 worker = st.radio("Hard=1 or Smart=0:",[0,1])
 team = st.radio("work in team ever?if yes choice 1",[1,0])
 mbti = st.radio("Are you Inrovert:if yes choice 1",[1,0])
 st.write("Your Logic, Hackaton, Coding Skill and Public Speaking points:",logic_rate,hack_rate,code_rate,speak_rate)
-st.write("Your selected:",self_learn , extra , certificate , workshop,rw_skill,memory_score,like_subj,like_area,like_com,
+st.write("Your selected:",self_learn,extra,certificate,workshop,rw_skill,memory_score,like_subj,like_area,like_com,
          takein_snr,like_book,mana_tech,worker,team,mbti)
         
        
