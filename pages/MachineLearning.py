faf05ba34c819fdb70c94a29db7d12ed78209390
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-17 13:10:08 +0630

    Update MachineLearning.py

--- pages/MachineLearning.py
+++ pages/MachineLearning.py
@@ -15,31 +15,31 @@ if self_learn == "yes":
          new_self = 1
 else :
          new_self = 0
-st.write(new_self)
+
 extra = st.radio("Extra Course:",['yes', 'no'])
 if extra == "yes":
          new_extra = 1
 else :
          new_extra = 0
-st.write(new_extra)
+#st.write(new_extra)
 snrinput = st.radio("Take in input from senior",['yes','no'])
 if snrinput == "yes":
          new_snrin = 1
 else :
          new_snrin = 0
-st.write(new_snrin)
+#st.write(new_snrin)
 team = st.radio("Work team in ever?",['yes','no'])
 if team == "yes":
          new_team = 1
 else :
          new_team = 0
-st.write(new_team)
+#st.write(new_team)
 mbti = st.radio("Are you introverted",['yes','no'])
 if mbti == "yes":
          new_mbti = 1
 else :
          new_mbti = 0
-st.write(new_mbti)
+#st.write(new_mbti)
 rdwe = st.selectbox("Reading or Writing Skill",['Excellent','Medium','Poor'])
 if rdwe == "Excellent":
          new_rdwe = 0
@@ -47,7 +47,7 @@ elif rdwe == "Medium":
          new_rdwe = 1
 else:
          new_rdwe = 2
-st.write(new_rdwe)
+#st.write(new_rdwe)
 memo = st.selectbox("Memory Capability Skill", ['Excellent','Medium','Poor'])
 if memo == "Excellent":
          new_memo = 0
@@ -55,61 +55,61 @@ elif memo == "Medium":
          new_memo = 1
 else:
          new_memo = 2
-st.write(new_memo)
+#st.write(new_memo)
 metl = st.radio("Choice Management or Technical",['Management', 'Technical']) 
 if metl == 'Management':
          new_metl = 0
 else:
          new_metl = 1
-st.write(new_metl)
+#st.write(new_metl)
 work = st.radio("Choice Smart or Hard",['Smart Worker','Hard Worker'])
 if work == 'Smart Worker':
          new_work = 0
 else:
          new_work = 1
-st.write(new_work)
+#st.write(new_work)
 certi = st.selectbox("Certificate",data['certifications'].unique()) 
 data['certifications']=data['certifications'].astype('category')
 d=dict(enumerate(data['certifications'].cat.categories))
 for key,value in d.items():
     if value == certi:
         new_certi=key
-st.write(new_certi)
+#st.write(new_certi)
 wshop = st.selectbox("Workshops",data['workshops'].unique())
 data['workshops']=data['workshops'].astype('category')
 w=dict(enumerate(data['workshops'].cat.categories))
 for key,value in w.items():
     if value == wshop:
         new_wshop=key
-st.write(new_wshop)
+#st.write(new_wshop)
 subj = st.selectbox("Interesting Subjects",data['Interested subjects'].unique())
 data['Interested subjects']=data['Interested subjects'].astype('category')
 s=dict(enumerate(data['Interested subjects'].cat.categories))
 for key,value in s.items():
     if value == subj:
         new_subj=key
-st.write(new_subj)
+#st.write(new_subj)
 area = st.selectbox("Interesting Working Area",data['interested career area '].unique())
 data['interested career area ']=data['interested career area '].astype('category')
 a=dict(enumerate(data['interested career area '].cat.categories))
 for key,value in a.items():
     if value == area:
         new_area=key
-st.write(new_area)
+#st.write(new_area)
 comp = st.selectbox("What type of company settle in",data['Type of company want to settle in?'].unique())
 data['Type of company want to settle in?']=data['Type of company want to settle in?'].astype('category')
 c=dict(enumerate(data['Type of company want to settle in?'].cat.categories))
 for key,value in c.items():
     if value == comp:
         new_comp=key
-st.write(new_comp)
+#st.write(new_comp)
 book = st.selectbox("Interesting type of book:",data['Interested Type of Books'].unique())
 data['Interested Type of Books']=data['Interested Type of Books'].astype('category')
 b=dict(enumerate(data['Interested Type of Books'].cat.categories))
 for key,value in b.items():
     if value == book:
         new_book=key
-st.write(new_book)
+#st.write(new_book)
 inputdata = {'Logical quotient rating': logic_rate,
                         'hackathons': hack_rate,                         
                         'coding skills rating': code_rate,
