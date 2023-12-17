ea0112865a4e88a4838d5a490afe94d71bb7d76c
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-17 13:44:15 +0630

    Update MachineLearning.py

--- pages/MachineLearning.py
+++ pages/MachineLearning.py
@@ -130,7 +130,7 @@ inputdata = {'Logical quotient rating': logic_rate,
                           'worked in teams ever?': new_team,
                            'Introvert': new_mbti}
 
-features = pd.DataFrame(inputdata, index=[0])
+features = pd.DataFrame(inputdata)
 st.write(features)
 df = data.select_dtypes(include=['object']).columns.tolist() 
-st.write(df)
+st.DataFrame(df)
